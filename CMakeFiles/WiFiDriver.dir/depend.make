002d81d7a5f1f5000294f884524bdd0063ea14d0
imagemlt <malingtao1019@163.com>
2024-10-22 21:18:40 +0800

    更新gitignore

--- CMakeFiles/WiFiDriver.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for WiFiDriver.
-# This may be replaced when dependencies are built.
--- CMakeFiles/WiFiDriver.dir/src/ParsedRadioPacket.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/WiFiDriver.dir/src/ParsedRadioPacket.cpp.o: \
-  /data/data/com.termux/files/home/devourer_tx/src/ParsedRadioPacket.cpp
